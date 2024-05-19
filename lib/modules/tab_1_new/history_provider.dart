2a75069506b9ccd8714fe7a1e074a0f32c9073e7
Abu <kifayat639.ku@gmail.com>
2024-05-19 12:16:43 +0500

    fixed sorting

--- lib/modules/tab_1_new/history_provider.dart
+++ lib/modules/tab_1_new/history_provider.dart
@@ -8,9 +8,15 @@ final historyProvider = FutureProvider<Map<DateTime, int>>((ref) async {
   Map<DateTime, int> points = {};
 
   File file = ref.read(dbFilesProvider).requireValue[1]![0];
-  var content = jsonDecode(await file.readAsString());
+  Map content = jsonDecode(await file.readAsString());
+  List dates = content.keys.toList();
+  dates.sort(
+    (a, b) {
+      return DateTime.parse(b).difference(DateTime.parse(a)).inSeconds;
+    },
+  );
 
-  for (String date in content.keys) {
+  for (String date in dates) {
     points[DateTime.parse(date)] = content[date]['points'];
   }
 
